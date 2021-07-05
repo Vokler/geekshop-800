363a9f66c340a1e71785df3fe8fe4b795318ec87
Vokler <valerypavlikov@yandex.ru>
2021-07-05 20:48:18 +0300

    added POST for profile controller

--- users/views.py
+++ users/views.py
@@ -35,7 +35,13 @@ def registration(request):
 
 
 def profile(request):
-    form = UserProfileForm(instance=request.user)
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
     context = {'title': 'GeekShop - Личный кабинет', 'form': form}
     return render(request, 'users/profile.html', context)
 
